52927c7d128cf1c986375fba6af59f89f70b73de
TruthFire <truth.fire@yandex.ru>
2023-04-30 18:50:01 +0300

    Added article list in ap

--- app/Http/Controllers/Admin/AdminArticlesController.php
+++ app/Http/Controllers/Admin/AdminArticlesController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\JssiArticle;
 use Illuminate\Http\Request;
 
 class AdminArticlesController extends Controller
 {
      public function index() {
-        return view('jssi.admin.pages.papers.articles');
+        $articles = JssiArticle::paginate(20);
+        return view('jssi.admin.pages.papers.articles', compact('articles'));
     }
 }
